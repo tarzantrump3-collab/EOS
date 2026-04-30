8b6a4c17d267f2fee7927e0957b1bff13b7c133f
tarzantrump3-collab <you@example.com>
2026-04-30 23:15:56 +0530

    hi

--- demo.c
+++ demo.c
@@ -2,5 +2,7 @@ git pull
 git add .
 git commit -m "update demo.c"
 git push
-git clone https://github.com/tarzantrump3-collab/EOS
-git config user.name tarzantrump3-collab
+git clone --global https://github.com/tarzantrump3-collab/EOS
+git config --global user.name tarzantrump3-collab
+hii how are you
+git config user.email
