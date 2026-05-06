d11b43fefb188f4cba2e988363d95fc6061c70d2
chetan1126 <chetankotrange11@gmail.com>
2026-05-06 09:37:25 +0530

    signals SIGSEGV

--- signal/signal_segv.c
+++ signal/signal_segv.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+void segfault_handler(int sig);
 
 void main(){
+	printf("^SIGSEGV\n");
 	signal(SIGSEGV, segfault_handler);
-
-	int *p + NULL;
-	*p = 10; //this tigger the SIGEGV
+	
+	printf("below of trigger function\n");
+	int *p = NULL;
+	*p = 10; //this tigger the SIGSEGV
+	printf("access invalid memory\n");
 
 	return;
 
 }
 
 void segfault_handler(int sig){
-	printf("error :segmentation fault (signal %d ) deced\n",sig);
+	printf("error :segmentation fault (signal %d ) detected\n",sig);
 	printf("cleaning up and existing safely ....\n");
 	exit(1); 
 }
